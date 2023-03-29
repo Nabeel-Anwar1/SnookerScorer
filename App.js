264e57b0fd110f5d40570577d16c0a4a3be35776
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2023-03-29 13:23:47 +0100

    basic setup

--- App.js
+++ App.js
@@ -1,10 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [score1, setScore1] = useState(0);
+  const [score2, setScore2] = useState(0);
+  const [player, setPlayer] = useState(1 || 2);
+
+  const setter = (val) => {
+    if (player == 1) {
+      setScore1(score1 + val);
+    } else {
+      setScore2(score2 + val);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Snooker Scorer</Text>
+      <Text>Player 1 Score: {score1}</Text>
+      <Text>Player 2 Score: {score2}</Text>
+      <Button
+        onPress={() => {
+          setter(1);
+        }}
+        title="1"
+      />
+      <Button
+        onPress={() => {
+          setter(2);
+        }}
+        title="2"
+      />
+      <Button
+        onPress={() => {
+          setter(3);
+        }}
+        title="3"
+      />
+      <Button
+        onPress={() => {
+          setter(4);
+        }}
+        title="4"
+      />
+      <Button
+        onPress={() => {
+          setter(5);
+        }}
+        title="5"
+      />
+      <Button
+        onPress={() => {
+          setter(6);
+        }}
+        title="6"
+      />
+      <Button
+        onPress={() => {
+          setter(7);
+        }}
+        title="7"
+      />
+      <Button
+        onPress={() => {
+          setPlayer(!player);
+        }}
+        title="Change Player"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +76,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    marginTop: 100,
+    marginRight: 50,
+    marginLeft: 50,
+    gap: 20,
   },
 });
