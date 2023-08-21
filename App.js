b7f2e1e19073e094cc6e71a8fc0b3152531d50fe
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2023-08-21 11:16:14 +0100

    moved states into score file so I didnt have to send it to children components

--- App.js
+++ App.js
@@ -4,26 +4,10 @@ import { useState } from "react";
 import Score from "./components/Score";
 
 export default function App() {
-  const [score1, setScore1] = useState(0);
-  const [score2, setScore2] = useState(0);
-  const [frame1, setFrame1] = useState(0);
-  const [frame2, setFrame2] = useState(0);
-  const [player, setPlayer] = useState(1 || 2);
 
   return (
     <View style={styles.container}>
-      <Score
-        score1={score1}
-        score2={score2}
-        setScore1={setScore1}
-        setScore2={setScore2}
-        frame1={frame1}
-        frame2={frame2}
-        setFrame1={setFrame1}
-        setFrame2={setFrame2}
-        player={player}
-        setPlayer={setPlayer}
-      />
+      <Score />
       <StatusBar style="false" />
     </View>
   );
--- components/Score.jsx
+++ components/Score.jsx
@@ -2,42 +2,47 @@ import {useState} from "react"
 import { StyleSheet, Text, View, Pressable, Modal, Alert } from "react-native";
 import ModalScore from "./Modal";
 
-const ScoreCards = (props) => {
+const ScoreCards = () => {
     const [modalVisible, setModalVisible] = useState(false)
+    const [score1, setScore1] = useState(0);
+    const [score2, setScore2] = useState(0);
+    const [frame1, setFrame1] = useState(0);
+    const [frame2, setFrame2] = useState(0);
+    const [player, setPlayer] = useState(1 || 2);
 
 return (
 <View >
     <View style={styles.box}>
    <ModalScore        
-        score1={props.score1}
-        score2={props.score2}
-        setScore1={props.setScore1}
-        setScore2={props.setScore2}
-        frame1={props.frame1}
-        frame2={props.frame2}
-        setFrame1={props.setFrame1}
-        setFrame2={props.setFrame2}
-        player={props.player}
-        setPlayer={props.setPlayer}
+        score1={score1}
+        score2={score2}
+        setScore1={setScore1}
+        setScore2={setScore2}
+        frame1={frame1}
+        frame2={frame2}
+        setFrame1={setFrame1}
+        setFrame2={setFrame2}
+        player={player}
+        setPlayer={setPlayer}
         setModalVisible={setModalVisible}
         modalVisible={modalVisible}/>
         <View style={styles.b1}>
-    <Pressable style={styles.button1} onPress={()=>{setModalVisible(true); props.setPlayer(1)}}>
-        <Text style={styles.score1}>{props.score1}</Text>
-        <Text style={styles.frames1}>Frames: {props.frame1}</Text>
+    <Pressable style={styles.button1} onPress={()=>{setModalVisible(true); setPlayer(1)}}>
+        <Text style={styles.score1}>{score1}</Text>
+        <Text style={styles.frames1}>Frames: {frame1}</Text>
     </Pressable>
     </View>
     <View style={styles.diffBox}>
-    <Text style={styles.diff}>{props.score1 - props.score2}</Text>
+    <Text style={styles.diff}>{score1 - score2}</Text>
     </View>
     <View style={styles.b2}>
-    <Pressable style={styles.button2} onPress={()=>{setModalVisible(true); props.setPlayer(2)}}>
-        <Text style={styles.score2}>{props.score2}</Text>
-        <Text style={styles.frames2}>Frames: {props.frame2}</Text>
+    <Pressable style={styles.button2} onPress={()=>{setModalVisible(true); setPlayer(2)}}>
+        <Text style={styles.score2}>{score2}</Text>
+        <Text style={styles.frames2}>Frames: {frame2}</Text>
     </Pressable>
     </View>
     <View style={styles.b3}>
-    <Pressable style={styles.reset} onPress={()=>{props.setFrame1(0); props.setFrame2(0); props.setScore1(0); props.setScore2(0)}}>
+    <Pressable style={styles.reset} onPress={()=>{setFrame1(0); setFrame2(0); setScore1(0); setScore2(0)}}>
         <Text style={styles.resetText}>Reset</Text>
      </Pressable>
      </View>
